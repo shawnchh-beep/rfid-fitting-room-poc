479821f02f5ea43e5f23a503b14521f4068c47b9
shawnchh-beep <shawn.chh@gmail.com>
2026-04-23 14:59:19 +0800

    Update supabase-client.js

--- public/js/supabase-client.js
+++ public/js/supabase-client.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = 'https://trgxtbqjkhydvbfndmhk.supabase.co'
+const SUPABASE_URL = 'https://rfid-fitting-room.supabase.co'
 const SUPABASE_ANON_KEY = 'sb_publishable_RjeQR-HU84MRCpByTqZlxg_lwJHStMP'
 
 window.supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
